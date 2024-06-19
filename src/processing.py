e5a467fab81ba32556a733668995ef813d6ebfdd
Рыбина Анастасия <20022002nr@gmail.com>
2024-06-19 13:10:43 +0300

    new functions

---
+++ src/processing.py
@@ -0,0 +1,22 @@
+from typing import List, Dict
+
+
+def filter_by_state(inform: List, state: str = "EXECUTED") -> List:
+    """Функция запускает цикл по принимает на вход список словарей и значение для ключа
+     и возвращает новый список, содержащий только те словари,
+     у которых ключ содержит переданное в функцию значение"""
+
+    new_list = []
+
+    for k in inform:
+        if k["state"] == state:
+            new_list.append(k)
+
+    return new_list
+
+
+def sort_by_date(date_list: List[Dict], reverse: bool = True) -> list[dict]:
+    """Функция сортирует списки словарей по по убыванию даты"""
+    sorted_list = sorted(date_list, key=lambda date_entry: date_entry["date"], reverse=reverse)
+
+    return sorted_list
