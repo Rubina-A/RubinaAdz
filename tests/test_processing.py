17431593a5b65f9836ed8581d05c674356a27437
Рыбина Анастасия <20022002nr@gmail.com>
2024-06-20 20:54:42 +0300

    Test processing

---
+++ tests/test_processing.py
@@ -0,0 +1,7 @@
+import pytest
+
+from src.processing import filter_by_state, sort_by_date
+
+def test_filter_by_state(my_list):
+    assert filter_by_state(my_list) == [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+                                        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]
