922403d36b328219e39f563ecaf5d1d0a524e49c
Рыбина Анастасия <20022002nr@gmail.com>
2024-06-20 19:06:06 +0300

    Test masks

---
+++ tests/test_masks.py
@@ -0,0 +1,21 @@
+import pytest
+
+from src.masks import get_mask_card_number, get_mask_account
+
+@pytest.mark.parametrize("card_number, mask_card_number", [
+    (7000792289606361, "7000 79** **** 6361"),
+    (7013852289602061, "7013 85** **** 2061"),
+    (2005792289606361, "2005 79** **** 6361"),
+    (7000792289601514, "7000 79** **** 1514"),
+])
+def test_get_mask_card_number(card_number, mask_card_number):
+    assert get_mask_card_number(card_number) == mask_card_number
+
+@pytest.mark.parametrize("account_number, mask_account_number", [
+    (73635108430135871515, "**1515"),
+    (65854108430135872585, "**2585"),
+    (73654108530135874310, "**4310"),
+    (73654108459135874205, "**4205"),
+])
+def test_get_mask_account(account_number, mask_account_number):
+    assert get_mask_account(account_number) == mask_account_number
