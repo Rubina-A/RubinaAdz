a4eca254303bdbde861bc83f189589759a467cad
Рыбина Анастасия <20022002nr@gmail.com>
2024-06-26 19:47:57 +0300

    test decorators

---
+++ tests/test_decorators.py
@@ -0,0 +1,14 @@
+import pytest
+
+from src.decorators import log, my_function
+
+def test_log():
+    with pytest.raises(TypeError):
+        my_function("1", 2)
+    with pytest.raises(ZeroDivisionError):
+        my_function(1, 0)
+
+def test_console_output(capsys):
+    result = my_function(3, 1)
+    captured = capsys.readouterr()
+    assert captured.out == "my_function ok\n"
