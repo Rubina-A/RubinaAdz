a12ac087e391cd27cb7eea3bf8603efab44f0717
Рыбина Анастасия <20022002nr@gmail.com>
2024-06-26 16:22:33 +0300

    test func in generators

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -2,11 +2,35 @@ from src.generators import filter_by_currency, transaction_descriptions, card_nu
 
 import pytest
 
-def test_transaction_descriptions(new_list):
-    assert transaction_descriptions(new_list) == "Перевод организации"
+def test_transaction_descriptions():
+    transactions = [
+        {"description": "Перевод организации"},
+        {"description": "Перевод со счета на счет"},
+        {"description": "Перевод со счета на счет"},
+        {"description": "Перевод с карты на карту"},
+        {"description": "Перевод организации"},
+    ]
 
-def test_filter_by_currency(new_list, "USD"):
-    assert transaction_descriptions(new_list) == 939719570
+    descriptions = list(transaction_descriptions(transactions))
+    expected = [
+        "Перевод организации",
+        "Перевод со счета на счет",
+        "Перевод со счета на счет",
+        "Перевод с карты на карту",
+        "Перевод организации"
+    ]
 
-def test_card_number_generator(0, 0):
-    assert card_number_generator(0, 0) == "0000 0000 0000 0001"
+    assert descriptions == expected
+
+def test_card_number_generator():
+    generator = card_number_generator(1, 10)
+    assert next(generator) == "0000 0000 0000 0001"
+    assert next(generator) == "0000 0000 0000 0002"
+    assert next(generator) == "0000 0000 0000 0003"
+    assert next(generator) == "0000 0000 0000 0004"
+    assert next(generator) == "0000 0000 0000 0005"
+    assert next(generator) == "0000 0000 0000 0006"
+    assert next(generator) == "0000 0000 0000 0007"
+    assert next(generator) == "0000 0000 0000 0008"
+    assert next(generator) == "0000 0000 0000 0009"
+    assert next(generator) == "0000 0000 0000 0010"
