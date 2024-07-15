d49ce3a18ff95bb0dbe197279daf60270fe13970
Рыбина Анастасия <20022002nr@gmail.com>
2024-07-15 19:01:30 +0300

    add log

--- src/masks.py
+++ src/masks.py
@@ -11,16 +11,22 @@ logger.setLevel(logging.DEBUG)
 def get_mask_card_number(card_number: int) -> str:
     """Принимает на вход номер карты и возвращает ее маску."""
     str_card_number = str(card_number)
-    mask_card_number = str_card_number[:4] + " " + str_card_number[4:6] + "** **** " + str_card_number[-4:]
-    logger.info(f'Принимается на вход номер карты {card_number} и маскируется')
-
-    return mask_card_number
+    if str_card_number.isdigit() and len(str_card_number) == 16:
+        mask_card_number = str_card_number[:4] + " " + str_card_number[4:6] + "** **** " + str_card_number[-4:]
+        logger.info(f'Принимается на вход номер карты {card_number} и маскируется')
+        return mask_card_number
+    else:
+        logger.error("Ошибка")
+        return ""
 
 
 def get_mask_account(account_number: int) -> str:
     """Принимает на вход номер счета и возвращает его маску."""
     str_account_number = str(account_number)
-    mask_account_number = "**" + str_account_number[-4:]
-    logger.info(f'Принимается на вход номер счета {account_number} и маскируется')
-
-    return mask_account_number
+    if str_account_number.isdigit() and len(str_account_number) == 20:
+        mask_account_number = "**" + str_account_number[-4:]
+        logger.info(f'Принимается на вход номер счета {account_number} и маскируется')
+        return mask_account_number
+    else:
+        logger.error("Ошибка")
+        return ""
